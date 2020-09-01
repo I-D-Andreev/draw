3e16d11e44d7ce908c0f58ec35dbd0f29cf13aba
Ivan <iivvaann.andreev@gmail.com>
2020-09-01 22:01:37 +0300

    Page will not be resized

--- mirror_draw/static/js/m_draw.js
+++ mirror_draw/static/js/m_draw.js
@@ -1,5 +1,5 @@
 import { MirrorCanvas } from './mirror_canvas.js'
 window.addEventListener('load', ()=> {
     var canvas_element = document.getElementById('md_canvas');
-    var canvas = new MirrorCanvas(canvas_element, window); // ??? maybe redo canvas on window size change
-})
+    var canvas = new MirrorCanvas(canvas_element, window);
+  })
