c58013cb736d41b0da937bde5c7900eed1cc2c22
Ivan <iivvaann.andreev@gmail.com>
2020-09-06 19:56:38 +0300

    Base AJAX send

--- mirror_draw/static/js/canvas_saver.js
+++ mirror_draw/static/js/canvas_saver.js
@@ -1,7 +1,33 @@
 class CanvasSaver {
-    constructor(mirror_canvas){
+    constructor(mirror_canvas, save_button) {
         this.canvas = mirror_canvas;
+        this.save_button = save_button;
+        this.save_button.addEventListener('click', this.save_image.bind(this));
     }
+
+    save_image(){
+        this.#send_to_db();
+    }
+
+    #send_to_db() {
+        var xh = new XMLHttpRequest();
+        
+        xh.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            // alert('Success');  
+            console.log('Success');
+          }
+          else {
+            //   alert('Error');
+            console.log('Error');
+          }
+        };
+
+        xh.open("PUT", "save", true);
+        xh.send();
+    }
+
+
 
 }
 
--- mirror_draw/static/js/m_draw.js
+++ mirror_draw/static/js/m_draw.js
@@ -2,11 +2,13 @@ import { MirrorCanvas } from './mirror_canvas.js'
 import { CanvasSaver } from './canvas_saver.js'
 window.addEventListener('load', ()=> {
     var canvas_element = document.getElementById('md_canvas');
-    var hl = document.getElementById('horizontal_line');
-    var vl = document.getElementById('vertical_line');
+    var horizontal_line = document.getElementById('horizontal_line');
+    var vertical_line = document.getElementById('vertical_line');
 
-    var hb = document.getElementById('horizontal_button');
-    var vb = document.getElementById('vertical_button');
-    var canvas = new MirrorCanvas(canvas_element, hl, vl, hb, vb, window);
-    var canvas_saver = new CanvasSaver(canvas);
+    var horizontal_button = document.getElementById('horizontal_button');
+    var vertical_button = document.getElementById('vertical_button');
+    var canvas = new MirrorCanvas(canvas_element, horizontal_line, vertical_line, horizontal_button, vertical_button, window);
+
+    var save_button = document.getElementById('save_button');
+    var canvas_saver = new CanvasSaver(canvas, save_button);
   })
