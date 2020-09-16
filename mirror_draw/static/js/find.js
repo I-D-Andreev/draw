874ee6ee8d16ca2faf53f86772f3eb6921343ba1
Ivan <iivvaann.andreev@gmail.com>
2020-09-16 21:06:10 +0300

    Draw on this image redirects with query parameter

--- mirror_draw/static/js/find.js
+++ mirror_draw/static/js/find.js
@@ -5,6 +5,8 @@ window.addEventListener('load', () => {
     var id_box = document.getElementById('id_textbox');
     var id_button = document.getElementById('id_button');
     var download_button = document.getElementById('download_button');
+    var draw_button = document.getElementById('draw_button');
+
     download_button.disabled = true;
 
     var img = document.getElementById('drawing');
@@ -19,16 +21,23 @@ window.addEventListener('load', () => {
 
     id_button.addEventListener('click', () => {
         var draw_id = id_box.value;
-        drawing_loader.load_drawing(draw_id);
+        drawing_loader.load_image(draw_id);
     });
 
-    img.addEventListener('load', ()=>{
+    img.addEventListener('load', () => {
         download_button.disabled = !img.src;
     });
 
     download_button.addEventListener('click', () => {
         if (img.src) {
             DrawingDownloader.download(img.src);
+        }
+    });
+
+    draw_button.addEventListener('click', () => {
+        var curr_id = drawing_loader.current_id;
+        if (curr_id) {
+            location.href = ('mirror_draw/?id=' + curr_id);
         }
     });
 })
