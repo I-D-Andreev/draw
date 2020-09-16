0b2b5e1fbc99333a7cdae518a65b3df8f1644d15
Ivan <iivvaann.andreev@gmail.com>
2020-09-16 20:47:54 +0300

    Extend drawing loader to load in canvas as well

--- mirror_draw/static/js/drawing_loader.js
+++ mirror_draw/static/js/drawing_loader.js
@@ -1,15 +1,27 @@
+const LOAD_TYPE = {
+    IMAGE: "image",
+    CANVAS: "canvas",
+}
+
 class DrawingLoader {
-    constructor(img) {
-        this.img = img;
+    constructor(img_element) { // either html img or html canvas
+        this.img = img_element;
+        this.current_id = null;
     }
 
-    load_drawing(drawing_id) {
+    load_image(drawing_id) {
         if (drawing_id.length > 0 && !isNaN(drawing_id)) {
-            this.#query_database(drawing_id);
+            this.#query_database(drawing_id, LOAD_TYPE.IMAGE);
         }
     }
 
-    #query_database(drawing_id) {
+    load_canvas(drawing_id) {
+        if (drawing_id.length > 0 && !isNaN(drawing_id)) {
+            this.#query_database(drawing_id, LOAD_TYPE.CANVAS);
+        }
+    }
+
+    #query_database(drawing_id, type) {
         var req = new Request('/draw/find/' + drawing_id,
             {
                 headers: { 'X-CSRFToken': csrftoken }
@@ -23,8 +35,20 @@ class DrawingLoader {
                 console.log('Received:', data);
 
                 if (data.success === true) {
-                    console.log('Successful load!');
-                    this.img.src = data.data;
+                    console.log('Successful load! Type: ' + type);
+                    if (type === LOAD_TYPE.IMAGE) {
+                        this.img.src = data.data;
+                        this.current_id = drawing_id;
+                    } else if (type === LOAD_TYPE.CANVAS) {
+                        //draw on a canvas
+                        var ctx = this.img.getContext('2d');
+                        var temp_img = new Image;
+                        temp_img.addEventListener('load', () => {
+                             ctx.drawImage(temp_img, 0, 0); 
+                        });
+                        temp_img.src = data.data;
+                    }
+
                 } else {
                     console.log('Response Error:', data.reason);
                     alert(data.reason);
