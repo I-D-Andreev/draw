ebd2de8cf39121901a09627c6c5b2c433b0b402c
Ivan <iivvaann.andreev@gmail.com>
2020-09-27 20:43:10 +0100

    Cant change line size from innerHTML

--- mirror_draw/static/js/colour_line_picker.js
+++ mirror_draw/static/js/colour_line_picker.js
@@ -3,19 +3,21 @@ class CoulourLinePicker {
         this.colour_picker = colour_picker;
         this.plus_button = plus_button;
         this.minus_button = minus_button;
-        this.line_size = line_size;
-        
+        this.line_size_el = line_size;
+        this.p_line_size = parseInt(this.line_size_el.innerHTML);
+
+
         this.plus_button.addEventListener('click', this.p_change_line_size.bind(this, 1));
         this.minus_button.addEventListener('click', this.p_change_line_size.bind(this, -1));
     }
 
     p_change_line_size(change){
-        let new_val = parseInt(this.line_size.innerHTML) + change;
-        this.line_size.innerHTML = Math.max(1, new_val);
+        this.p_line_size = Math.max(1, this.p_line_size + change);
+        this.line_size_el.innerHTML = this.p_line_size;
     }
 
     get_size(){
-        return parseInt(this.line_size.innerHTML);
+        return this.p_line_size;
     }
 
     get_colour(){
